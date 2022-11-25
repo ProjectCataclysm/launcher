838ccad3da49fd534f297fd9df5c95d8f99a6e03
Mihail <slyfa34@gmail.com>
2022-11-25 12:48:51 +0300

    Исправил краш при выборе цвета источника света в настройках декора

---
+++ src/main/java/cataclysm/launcher/download/torrent/TorrentDispatcher.java
@@ -0,0 +1,21 @@
+package cataclysm.launcher.download.torrent;
+
+/**
+ * <br><br>REVOM ENGINE / ProjectCataclysm
+ * <br>Created: 24.11.2022 9:28
+ *
+ * @author Knoblul
+ */
+public class TorrentDispatcher {
+//	private static final boolean available;
+//
+//	public static boolean isAvailable() {
+//
+//	}
+//
+//	static {
+//		if (PlatformHelper.getPlatform() == PlatformHelper.Platform.WINDOWS) {
+//
+//		}
+//	}
+}
